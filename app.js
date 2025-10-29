ce7491488674c31008c66e9b130c7d33233e2164
Ingrid Silva <ingridsilvatec@outlook.com>
2025-10-29 16:43:27 -0300

    adiciona break

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ alert('Boas vindas ao jogo do número secreto!');
 
 let numeroSecreto = 2;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
 // se chute for igual ao número secreto 
-
-if (chute == numeroSecreto) {
-    alert('Isso ai! voce descobriu o número secreto ${numeroSecreto}!');
-} else {
-    if (chute > numeroSecreto) {
-        alert('O número secreto é maior que ${chute}!');
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        break;
     } else {
-        alert('O número secreto é menor que ${chute}!');
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é maior que ${chute}!`);
+        } else {
+            alert(`O número secreto é menor que ${chute}!`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
     }
+}
+
+if (tentativas > 1) {
+    alert(`Isso ai! voce descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+} else {
+    alert(`Isso ai! voce descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
 }
