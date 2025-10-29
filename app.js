89c152bc386c9bb2e34e868adbd2b7785b686315
Ingrid Silva <ingridsilvatec@outlook.com>
2025-10-29 16:18:22 -0300

    adiciona projeto

---
+++ app.js
@@ -0,0 +1,17 @@
+alert('Boas vindas ao jogo do número secreto!');
+
+let numeroSecreto = 2;
+console.log(numeroSecreto);
+let chute = prompt('Escolha um número entre 1 e 10');
+
+// se chute for igual ao número secreto 
+
+if (chute == numeroSecreto) {
+    alert('Isso ai! voce descobriu o número secreto ${numeroSecreto}!');
+} else {
+    if (chute > numeroSecreto) {
+        alert('O número secreto é maior que ${chute}!');
+    } else {
+        alert('O número secreto é menor que ${chute}!');
+    }
+}
