c69532dce3642cc91677e91843dc0db7e310a5ae
Ingrid Silva <ingridsilvatec@outlook.com>
2025-10-29 17:22:46 -0300

    projeto final

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 alert('Boas vindas ao jogo do número secreto!');
-
-let numeroSecreto = 2;
+let numeroMaximo = 256;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // se chute for igual ao número secreto 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}: `);
     if (chute == numeroSecreto) {
         break;
     } else {
         if (chute > numeroSecreto) {
-            alert(`O número secreto é maior que ${chute}!`);
-        } else {
             alert(`O número secreto é menor que ${chute}!`);
+        } else {
+            alert(`O número secreto é maior que ${chute}!`);
         }
         //tentativas = tentativas + 1;
         tentativas++;
