eb31091fa78f5128613537757dceea2bdb46923e
Ingrid Silva <ingridsilvatec@outlook.com>
2025-11-27 12:14:55 -0300

    versao 2 inicial

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-alert('Boas vindas ao jogo do número secreto!');
-let numeroMaximo = 256;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
 
-// se chute for igual ao número secreto 
-while (chute != numeroSecreto) {
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}: `);
-    if (chute == numeroSecreto) {
-        break;
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}!`);
-        } else {
-            alert(`O número secreto é maior que ${chute}!`);
-        }
-        //tentativas = tentativas + 1;
-        tentativas++;
-    }
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 a 256';
+
+function verificarChute() {
+    console.log('O botao foi clicado');
 }
-
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
-
